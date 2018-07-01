efaae8c8ad24526203aa2f38b74b1eede9275acd
Peter Traversa <peter.traversa@gmail.com>
2018-07-01 19:48:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   let newArray = [];
   do {
-    newArray.push("I love the Beatles!"[number]); number++;
+    newArray.push("I love the Beatles!"); number++;
   } while (number < 15);
   return newArray;
 }
